150f3e0a6e5db41ffb36a33fd2de4d37267f7ce8
Eileen S <eileen.j.sul@gmail.com>
2018-10-30 13:39:41 -0500

    updated JS and HTML

---
+++ assets/javascript/app.js
@@ -0,0 +1,28 @@
+// onload function
+window.onload = function () {
+    $("#start").click(startGame);
+};
+// declare variables
+var question1="";
+var question2="";
+var question3="";
+var question4="";
+var question5="";
+var question6="";
+var correct="";
+var incorrect="";
+var unanswered="";
+
+function startGame (){
+
+}
+
+function newGame (){
+    
+}
+    // timer (2 min)
+// new game function
+    // radio button
+    // 6 questions
+    // correct, incorrect, unanswered
+// call the functions
