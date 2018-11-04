67117b6431bb1b47eaa4bc56a1c48726e70323b3
Eileen S <eileen.j.sul@gmail.com>
2018-11-03 23:51:05 -0500

    final

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,33 +1,57 @@
 // onload function
 window.onload = function () {
-    $("#start").click(startGame);
+    $("#start").on("click", timer1.start);
+    $("#start").on("click", myFunction);
+    $("#start").on("click", startGame);
 };
-// declare variables
-var question1="";
-var question2="";
-var question3="";
-var question4="";
-var question5="";
-var question6="";
-var correct="";
-var incorrect="";
-var unanswered="";
+
+// Initialize game
+var correct = 0;
+var incorrect = 0;
+var unanswered = 0;
+var clockRunning = true;
 
 function myFunction() {
     var x = document.getElementById("myRadio");
     x.checked = true;
 }
 
-function startGame (){
+function startGame() {
+
+    if (".row" == true) {
+        showCorrect++;
+    }
+    else if (".row" == false) {
+        showIncorrect++;
+    }
+    else if (".row" == 0) {
+        showUnanswered++;
+    }
 
 }
 
-function newGame (){
-    
+var startTime = Date.now()
+
+function updateTimer() {
+    var timeElapsed = Date.now() - startTime
+    var timeLeft = Math.floor((20000 - timeElapsed) / 1000)
+    if(timeLeft < 0) {
+        return endGame()
+    }
+    $('#timer').text("Seconds left: " + timeLeft)
 }
-    // timer (2 min)
-// new game function
-    // radio button
-    // 6 questions
-    // correct, incorrect, unanswered
-// call the functions
+
+function endGame() {
+    window.location.href = "endpage.html"
+    document.getElementById("showCorrect").textContent = correct;
+    document.getElementById("showIncorrect").textContent = incorrect;
+    document.getElementById("showUnanswered").textContent = unanswered;
+}
+
+setInterval(function() {
+    updateTimer()
+}, 100)
+
+myFunction();
+startGame();
+
